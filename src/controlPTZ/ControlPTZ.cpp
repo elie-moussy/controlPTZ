65df635e9c3b5bf7eda6fd9a82a1267c171244f7
elie-moussy <elie.moussy@laas.fr>
2014-05-23 16:35:10 +0200

    Add documentation.

--- src/controlPTZ/ControlPTZ.cpp
+++ src/controlPTZ/ControlPTZ.cpp
@@ -1,211 +1,245 @@
 #include "controlPTZ/ControlPTZ.h"
 
 using namespace std;
+///\class ControlPTZ ControlPTZ.h "controlPTZ/ControlPTZ.h"
+///\brief This class allows to manipulate the PTZ cameras.
 
+///\sa ControlPTZ::ControlPTZ();
+///\brief Constructor for the class "ControlPTZ". Once it is called, it will initialize curl.
 ControlPTZ::ControlPTZ()
 {
-	//Curl configuration to work with HTTPS protocole
-	handle=curl_easy_init();
-	curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0L);
-	curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 0L);
-  	curl_easy_setopt(handle, CURLOPT_NOPROGRESS, 1L);
-  	curl_easy_setopt(handle, CURLOPT_USERPWD, "root:axis0");
-  	curl_easy_setopt(handle, CURLOPT_USERAGENT, "curl/7.21.6 (i686-pc-linux-gnu) libcurl/7.21.6 OpenSSL/1.0.0e zlib/1.2.3.4 libidn/1.22 librtmp/2.3");
-  	curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 1L);
-  	curl_easy_setopt(handle, CURLOPT_MAXREDIRS, 50L);
-  	curl_easy_setopt(handle, CURLOPT_HTTPAUTH, -17L);
-	
+  //Curl configuration to work with HTTPS protocole
+  handle=curl_easy_init();
+  curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0L);
+  curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 0L);
+  curl_easy_setopt(handle, CURLOPT_NOPROGRESS, 1L);
+  curl_easy_setopt(handle, CURLOPT_USERPWD, "root:axis0");
+  curl_easy_setopt(handle, CURLOPT_USERAGENT, "curl/7.21.6 (i686-pc-linux-gnu) libcurl/7.21.6 OpenSSL/1.0.0e zlib/1.2.3.4 libidn/1.22 librtmp/2.3");
+  curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 1L);
+  curl_easy_setopt(handle, CURLOPT_MAXREDIRS, 50L);
+  curl_easy_setopt(handle, CURLOPT_HTTPAUTH, -17L); 
 }
 
+///\sa ControlPTZ::~ControlPTZ();
+///\brief Desstructor for the class "ControlPTZ". Once it is called, it will cleanup curl.
 ControlPTZ::~ControlPTZ()
 {
-	curl_easy_cleanup(handle);
+  curl_easy_cleanup(handle);
 }
 
+///\sa void ControlPTZ::HTTPRequestPTZ(int pan, int tilt, int zoom, int cam);
+///\brief This function of the class "ControlPTZ" will allow the camera to do a continuous move with a specified pan step, tilt step and zoom step.
+///\param pan Input value of pan step.
+///\param tilt Input value of tilt step.
+///\param zoom Input value of zoom step.
+///\param cam Input value to specify the camera (1 or 2).
 void ControlPTZ::HTTPRequestPTZ(int pan, int tilt, int zoom, int cam)
 {
-	ostringstream oss;
-
-	if (pan>100)
-	  pan=100;
-	else if (pan<-100)
-	  pan=-100;
-	
-	if (tilt>100)
-	  tilt=100;
-	else if (tilt<-100)
-	  tilt=-100;
-
-	if (zoom>9999)
-	  zoom=9999;
+  ostringstream oss;
+  
+  if (pan>100)
+    pan=100;
+  else if (pan<-100)
+    pan=-100;
+  
+  if (tilt>100)
+    tilt=100;
+  else if (tilt<-100)
+    tilt=-100;
+  
+  if (zoom>9999)
+    zoom=9999;
 	else if (zoom<-9999)
 	  zoom=-9999;
-
-	if (cam == 1 || cam == 2)
-	{
-		oss << "http://axis-ptz" << cam << "/axis-cgi/com/ptz.cgi?continuouspantiltmove=";
-		oss << pan;
-		oss << ",";
-		oss << tilt;
-		oss << "&rzoom=" << zoom;
-		curl_easy_setopt(handle, CURLOPT_URL, oss.str().c_str());
-		curl_easy_perform(handle);
-	}
+  
+  if (cam == 1 || cam == 2)
+    {
+      oss << "http://axis-ptz" << cam << "/axis-cgi/com/ptz.cgi?continuouspantiltmove=";
+      oss << pan;
+      oss << ",";
+      oss << tilt;
+      oss << "&rzoom=" << zoom;
+      curl_easy_setopt(handle, CURLOPT_URL, oss.str().c_str());
+      curl_easy_perform(handle);
+    }
 }
 
+///\sa void ControlPTZ::HTTPRequestPTZPosRelative(float pan, float tilt, float zoom, int cam);
+///\brief This function of the class "ControlPTZ" will allow the camera to go to a relative position with a specified pan step, tilt step and zoom step.
+///\param pan Input value of pan step.
+///\param tilt Input value of tilt step.
+///\param zoom Input value of zoom step.
+///\param cam Input value to specify the camera (1 or 2).
 void ControlPTZ::HTTPRequestPTZPosRelative(float pan, float tilt, float zoom, int cam)
 {
-	ostringstream oss;
-
-	if (pan>360)
-	  pan=360;
-	else if (pan<-360)
-	  pan=-360;
-	
-	if (tilt>360)
-	  tilt=360;
-	else if (tilt<-360)
-	  tilt=-360;
-
-	if (zoom>9999)
-	  zoom=9999;
-	else if (zoom<-9999)
-	  zoom=-9999;
-
-	if (cam == 1 || cam == 2)
-	{
-		oss << "http://axis-ptz" << cam << "/axis-cgi/com/ptz.cgi?rpan=" << pan;
-		oss << "&rtilt=" << tilt;
-		oss << "&rzoom=" << zoom;
-
-		curl_easy_setopt(handle, CURLOPT_URL, oss.str().c_str());
-		curl_easy_perform(handle);
-	}
+  ostringstream oss;
+  
+  if (pan>360)
+    pan=360;
+  else if (pan<-360)
+    pan=-360;
+  
+  if (tilt>360)
+    tilt=360;
+  else if (tilt<-360)
+    tilt=-360;
+  
+  if (zoom>9999)
+    zoom=9999;
+  else if (zoom<-9999)
+    zoom=-9999;
+  
+  if (cam == 1 || cam == 2)
+    {
+      oss << "http://axis-ptz" << cam << "/axis-cgi/com/ptz.cgi?rpan=" << pan;
+      oss << "&rtilt=" << tilt;
+      oss << "&rzoom=" << zoom;
+	    
+      curl_easy_setopt(handle, CURLOPT_URL, oss.str().c_str());
+      curl_easy_perform(handle);
+    }
 }
 
+///\sa void ControlPTZ::HTTPRequestPTZPosAbsolute(float pan, float tilt, float zoom, int cam);
+///\brief This function of the class "ControlPTZ" will allow the camera to go to an absolute position with a specified pan step, tilt step and zoom step.
+///\param pan Input value of pan step.
+///\param tilt Input value of tilt step.
+///\param zoom Input value of zoom step.
+///\param cam Input value to specify the camera (1 or 2).
 void ControlPTZ::HTTPRequestPTZPosAbsolute(float pan, float tilt, float zoom, int cam)
 {
-	ostringstream oss;
-
-	if (pan>180)
-	  pan=180;
-	else if (pan<-180)
-	  pan=-180;
-	
-	if (tilt>180)
-	  tilt=180;
-	else if (tilt<-180)
-	  tilt=-180;
-
-	if (zoom>9999)
-	  zoom=9999;
-	else if (zoom<-9999)
-	  zoom=-9999;
-
-	if (cam == 1 || cam == 2)
-	{
-		oss << "http://axis-ptz" << cam << "/axis-cgi/com/ptz.cgi?pan=" << pan;
-		oss << "&tilt=" << tilt;
-		oss << "&zoom=" << zoom;
-
-		curl_easy_setopt(handle, CURLOPT_URL, oss.str().c_str());
-		curl_easy_perform(handle);
-	}
+  ostringstream oss;
+  
+  if (pan>180)
+    pan=180;
+  else if (pan<-180)
+    pan=-180;
+  
+  if (tilt>180)
+    tilt=180;
+  else if (tilt<-180)
+    tilt=-180;
+  
+  if (zoom>9999)
+    zoom=9999;
+  else if (zoom<-9999)
+    zoom=-9999;
+  
+  if (cam == 1 || cam == 2)
+    {
+      oss << "http://axis-ptz" << cam << "/axis-cgi/com/ptz.cgi?pan=" << pan;
+      oss << "&tilt=" << tilt;
+      oss << "&zoom=" << zoom;
+      
+      curl_easy_setopt(handle, CURLOPT_URL, oss.str().c_str());
+      curl_easy_perform(handle);
+    }
 }
 
+///\sa void ControlPTZ::HTTPRequestSetAutoFocus(bool b);
+///\brief This function of the class "ControlPTZ" will allow both cameras to activate/deactivate the auto focus.
+///\param b Input value. If it is true, the auto focus will be activated. Otherwise, it will be deactivated.
 void ControlPTZ::HTTPRequestSetAutoFocus(bool b)
 {
-	ostringstream oss1, oss2;
-	
-	if (b)
-	{
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autofocus=on";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autofocus=on";
+  ostringstream oss1, oss2;
+  
+  if (b)
+    {
+      oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autofocus=on";
+      oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autofocus=on";
+      
+      curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+      curl_easy_perform(handle);
+      curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+      curl_easy_perform(handle);
+    } else {
+    oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autofocus=off";
+    oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autofocus=off";
 
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	} else {
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autofocus=off";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autofocus=off";
-
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	}
+    curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+    curl_easy_perform(handle);
+    curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+    curl_easy_perform(handle);
+  }
 }
 
+///\sa void ControlPTZ::HTTPRequestSetAutoIris(bool b);
+///\brief This function of the class "ControlPTZ" will allow both cameras to activate/deactivate the auto iris.
+///\param b Input value. If it is true, the auto iris will be activated. Otherwise, it will be deactivated.
 void ControlPTZ::HTTPRequestSetAutoIris(bool b)
 {
-	ostringstream oss1, oss2;
+  ostringstream oss1, oss2;
 	
-	if (b)
-	{
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autoiris=on";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autoiris=on";
+  if (b)
+    {
+      oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autoiris=on";
+      oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autoiris=on";
 
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	} else {
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autoiris=off";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autoiris=off";
+      curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+      curl_easy_perform(handle);
+      curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+      curl_easy_perform(handle);
+    } else {
+    oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?autoiris=off";
+    oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?autoiris=off";
 
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	}
+    curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+    curl_easy_perform(handle);
+    curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+    curl_easy_perform(handle);
+  }
 }
 
+///\sa void ControlPTZ::HTTPRequestSetAutoIRCutFilter(bool b);
+///\brief This function of the class "ControlPTZ" will allow both cameras to activate/deactivate the IR cut filter.
+///\param b Input value. If it is true, the IR cut filter will be activated. Otherwise, it will be deactivated.
 void ControlPTZ::HTTPRequestSetIRCutFilter(bool b)
 {
-	ostringstream oss1, oss2;
+  ostringstream oss1, oss2;
 	
-	if (b)
-	{
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?ircutfilter=on";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?ircutfilter=on";
+  if (b)
+    {
+      oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?ircutfilter=on";
+      oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?ircutfilter=on";
 
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	} else {
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?ircutfilter=off";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?ircutfilter=off";
+      curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+      curl_easy_perform(handle);
+      curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+      curl_easy_perform(handle);
+    } else {
+    oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?ircutfilter=off";
+    oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?ircutfilter=off";
 
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	}
+    curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+    curl_easy_perform(handle);
+    curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+    curl_easy_perform(handle);
+  }
 }
 
+///\sa void ControlPTZ::HTTPRequestSetAutoBackLight(bool b);
+///\brief This function of the class "ControlPTZ" will allow both cameras to activate/deactivate the auto back light.
+///\param b Input value. If it is true, the auto back light will be activated. Otherwise, it will be deactivated.
 void ControlPTZ::HTTPRequestSetAutoBackLight(bool b)
 {
-	ostringstream oss1, oss2;
-	
-	if (b)
-	{
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?backlight=on";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?backlight=on";
-
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	} else {
-		oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?backlight=off";
-		oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?backlight=off";
-
-		curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
-		curl_easy_perform(handle);
-		curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
-		curl_easy_perform(handle);
-	}
+  ostringstream oss1, oss2;
+  
+  if (b)
+    {
+      oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?backlight=on";
+      oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?backlight=on";
+      
+      curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+      curl_easy_perform(handle);
+      curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+      curl_easy_perform(handle);
+    } else {
+    oss1 << "http://axis-ptz1/axis-cgi/com/ptz.cgi?backlight=off";
+    oss2 << "http://axis-ptz2/axis-cgi/com/ptz.cgi?backlight=off";
+    
+    curl_easy_setopt(handle, CURLOPT_URL, oss1.str().c_str());
+    curl_easy_perform(handle);
+    curl_easy_setopt(handle, CURLOPT_URL, oss2.str().c_str());
+    curl_easy_perform(handle);
+  }
 }
-
--- src/controlPTZ/StreamRTSP.cpp
+++ src/controlPTZ/StreamRTSP.cpp
@@ -2,74 +2,91 @@
 
 using namespace std;
 
+///\class StreamRTSP StreamRTSP.h "controlPTZ/StreamRTSP.h"
+///\brief This class allows to grab a frame from a device.
+
+///\sa StreamRTSP::StreamRTSP(string pathRTSP, int fps, bool iplImage);
+///\brief The constructor of the class "StreamRTSP".
+///\param pathRTSP Input value of the stream path.
+///\param fps Input value of the number of frames per second.
+///\param iplImage Input value. If "iplImage" is true, we use the old version of opencv in order to handle the frame. Otherwise, we use the new version of opencv.
 StreamRTSP::StreamRTSP(string pathRTSP, int fps, bool iplImage)
 {
-	this->iplImage=iplImage;
+  this->iplImage=iplImage;
 
-	ostringstream oss;
-	oss << pathRTSP;
-	if (fps != -1)
-	{
-	  oss << "?fps=" << fps;
-	  this->fps=fps;
-	}
+  ostringstream oss;
+  oss << pathRTSP;
+  if (fps != -1)
+    {
+      oss << "?fps=" << fps;
+      this->fps=fps;
+    }
 	
-	cout << "Open the stream (this might take several minutes...)\nWait the frame window" << endl;
+  cout << "Open the stream (this might take several minutes...)\nWait the frame window" << endl;
 	
-	if (this->iplImage)
-	{
-	  cvCap=cvCaptureFromFile(oss.str().c_str());
+  if (this->iplImage)
+    {
+      cvCap=cvCaptureFromFile(oss.str().c_str());
 
-	  if(cvCap==NULL)
-	  {
-        	cout << "Error opening video stream or file" << endl;
-    	  }
-	  currentFrame = this->grabIplFrame();
-	}
-	else
+      if(cvCap==NULL)
 	{
-	  vcap.open(oss.str().c_str());
-	  matCurrentFrame = this->grabFrame();
+	  cout << "Error opening video stream or file" << endl;
 	}
-
-	
+      currentFrame = this->grabIplFrame();
+    }
+  else
+    {
+      vcap.open(oss.str().c_str());
+      matCurrentFrame = this->grabFrame();
+    }	
 }
 
+///\sa StreamRTSP::StreamRTSP(int device);
+///\brief The constructor of the class "StreamRTSP".
+///\param device Input value that specifies the device from which the frame should be grabed.
 StreamRTSP::StreamRTSP(int device)
 {
-	vcap.open(device);
-	this->iplImage=false;
-	matCurrentFrame = this->grabFrame();
+  vcap.open(device);
+  this->iplImage=false;
+  matCurrentFrame = this->grabFrame();
 }
 
+///\sa StreamRTSP::StreamRTSP(int device);
+///\brief The desstructor of the class "StreamRTSP".
 StreamRTSP::~StreamRTSP()
 {
-	if (this->iplImage)
-	  cvReleaseCapture(&cvCap);
+  if (this->iplImage)
+    cvReleaseCapture(&cvCap);
 }
 
+///\sa IplImage *StreamRTSP::grabIplFrame();
+///\brief This function of the class "StreamRTSP" allows to grab a frame from a device.
+///\return This function will return the frame grabbed via an IplImage pointer (old version of opencv).
 IplImage *StreamRTSP::grabIplFrame()
 {
 
-	currentFrame=cvQueryFrame(cvCap);
-	if (!currentFrame)
-	{
-	  cout << "StreamRTSP : grab frame error" << endl;
-	}
-	return currentFrame;
+  currentFrame=cvQueryFrame(cvCap);
+  if (!currentFrame)
+    {
+      cout << "StreamRTSP : grab frame error" << endl;
+    }
+  return currentFrame;
 }
 
+///\sa Mat *StreamRTSP::grabFrame();
+///\brief This function of the class "StreamRTSP" allows to grab a frame from a device.
+///\return This function will return the frame grabbed via an opencv matrix (new version of opencv).
 Mat StreamRTSP::grabFrame()
 {
 
-	if (!vcap.read(matCurrentFrame))
-	{
-	  cout << "StreamRTSP : grab frame error" << endl;
-	}
-	else
-	{
-		matLastFrame=matCurrentFrame;
-	}
-	return matLastFrame;
+  if (!vcap.read(matCurrentFrame))
+    {
+      cout << "StreamRTSP : grab frame error" << endl;
+    }
+  else
+    {
+      matLastFrame=matCurrentFrame;
+    }
+  return matLastFrame;
 }
 
